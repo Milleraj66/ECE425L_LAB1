bbdd44015c022fc9b6527308f273091bf2b52070
milleraj66 <milleraj66@gmail.com>
2015-10-03 21:28:35 -0700

    Test commit for git with Vivado

---
+++ ECE425L_LAB1.srcs/sources_1/new/Mux16bit_2to1.v
@@ -0,0 +1,16 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// AUTHOR: ARTHUR J. MILLER
+// Create Date: 10/03/2015 08:58:39 PM
+// ECE425L LAB #1, Problem 1
+// Purpose: Develope a Verilog structural model for a 16-bit 2-to-1 MUX and a 3-to-8
+//          decoder includeing ENABLE signals. Develop a testbench, perofrm a waveform
+//          simulation, and demonstrate the output to the Instructor.
+
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Mux16bit_2to1(
+
+    );
+endmodule
